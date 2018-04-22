a720f57d727c696d676c4085f6ff66fe54bcb125
Adriano Oliveira <drprado2@gmail.com>
2018-04-22 16:43:50 -0300

    Teste Travis CI

--- src/EasyDesignPatterns.Tests/UnitTest1.cs
+++ src/EasyDesignPatterns.Tests/UnitTest1.cs
@@ -12,5 +12,42 @@ namespace EasyDesignPatterns.Tests
             var a= 245;
             a.Should().Be(255);
         }
+        
+        [Fact]
+        public void meu_teste_top()
+        {
+            var a= 245;
+            a.Should().Be(245);
+        }
+        [Fact]
+        public void outro_teste_maluco()
+        {
+            var a= 245;
+            a.Should().Be(245);
+        }
+        [Fact]
+        public void mais_um_teste()
+        {
+            var a= 245;
+            a.Should().Be(245);
+        }
+        [Fact]
+        public void outro_teste_doido()
+        {
+            var a= 245;
+            a.Should().Be(255);
+        }
+        [Fact]
+        public void esse_teste_e_bom()
+        {
+            var a= 245;
+            a.Should().Be(255);
+        }
+        [Fact]
+        public void teste_testando_passou()
+        {
+            var a= 245;
+            a.Should().Be(246);
+        }
     }
 }
